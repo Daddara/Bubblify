42119b15d50fad33ae12aed23fe3c87944b684dd
daddara <arnarrvk@gmail.com>
2022-03-14 13:45:32 +0000

    basic final confirm

--- src/components/OrderReview/index.js
+++ src/components/OrderReview/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./styles.css";
+
 
 
 class OrderReview extends React.Component{
@@ -9,19 +11,55 @@ class OrderReview extends React.Component{
 
     submit(e){
         console.log("Submit");
-    }
+    };
 
+ 
     render(){
+        console.log("hello", this.state.order)
         return(
             // Render here a sort of list of the cart
-            <div><NavLink
-            className="checkoutbtn"
-            onClick={e => this.submit(e)}
-            // activeClassName="is-active"
-            to="/order/success"
-            exact>
-        Confirm
-        </NavLink></div>
+            
+            <div>
+            <h1 className="h1Bundles">Cart Items</h1>
+            <div className="cartDiv">
+                {this.state.order &&
+                this.state.order.map((item, index) => {
+
+                if(item.hasOwnProperty("bundle")){
+            return (
+                    <div key={item.bundle.id}  className="CartItem">
+                        <div>
+                            <h2 className="Heading">{item.bundle.counter} x {item.bundle.name}</h2>
+                            <p className="BubblePrice">Price: {item.bundle.price} kr</p>
+                        </div>
+                    </div>
+                );
+                }
+                else{
+                    console.log("hello")
+                    return (
+                    <div key={item.bubble.id} className="CartItem">
+                        <div>
+                            <h2 className="Heading">{item.bubble.counter} x {item.bubble.name}</h2>
+                            <p className="BubblePrice">Price: {item.bubble.price} kr</p>
+                        </div>
+                    </div>
+                );
+                }
+                })}
+                <NavLink className="checkoutbtn" to="/cart/checkout" exact>Proceed to Checkout</NavLink>  
+            </div>
+        </div>  
+
+
+        //     <div><NavLink
+        //     className="checkoutbtn"
+        //     onClick={e => this.submit(e)}
+        //     // activeClassName="is-active"
+        //     to="/order/success"
+        //     exact>
+        // Confirm
+        // </NavLink></div>
         )
     }
 }
